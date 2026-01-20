3583af8432718c69ed57c6f17f1a20bc2f25aeba
thealing <159199097+thealing@users.noreply.github.com>
2026-01-20 21:54:06 +0100

    Fixed build file

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-@ g++ -O2 -s main.cpp -o main.exe -static-libgcc -l gdi32 -l ole32 -l oleaut32 -l uuid
+@ g++ -O2 -s main.cpp -o main.exe -static-libgcc -l gdi32 -l ole32 -l oleaut32 -l uuid -lwindowscodecs
